131d6c78768abe292cff5312e2b5ba96ce27eadf
164403-maker <164403@wsk.ac.th>
2026-06-18 08:19:56 +0000

    Praogram02.py

---
+++ Praogram02.py
@@ -0,0 +1,9 @@
+width = float(input("Enter width: "))
+length = float(input("Enter length: "))
+
+area = width * length
+perimeter = 2 * (width + length)
+
+print("Calculation Result")
+print("Area =", area)
+print("Perimeter =", perimeter)
